63cbe43a8adb65e4743cc610018dbba1dcb14fa2
Олександр Стаднік <sanyokstd@gmail.com>
2020-07-30 17:08:46 +0300

    1
    768

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,49 @@
 $(function () {
-	var slideIndexBis = 1;
-	var sliding = false;
-	let myFullpage = new fullpage('#full')
+	let myFullpage = new fullpage('#full',{
+		responsiveWidth: 992
+	});
 
 	$('.arr-d').click(function () {
 		myFullpage.moveSectionDown();
+		myFullpage.fitToSection();
+
 	})
 	$('.arr-up').click(function () {
 		myFullpage.moveSectionUp();
+	})
+
+	function destroyFullpage(){
+		if($(window).width() < 992){
+			myFullpage.destroy('all');
+			console.log('1')
+		}
+	}
+	
+	$(window).resize(function(){
+		destroyFullpage()
+	})
+	destroyFullpage()
+
+	$(window).scroll(function() {
+
+		var target = $(this).scrollTop();
+
+		if($(window).width() < 992){
+			if(target == 0) {
+
+				$('#menu').removeClass('white')
+	
+			} else {
+	
+				$('#menu').addClass('white')
+	
+			}
+		}
+	
+	});
+
+	$('.burger').click(function(){
+		
 	})
 })
 $(document).ready(function () {
