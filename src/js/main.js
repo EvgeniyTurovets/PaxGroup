17e0e168742e69ecf0d038a3e9a0c8ddf956cdbf
Олександр Стаднік <sanyokstd@gmail.com>
2020-07-30 15:12:30 +0300

    1
    1

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,18 @@
 $(function () {
-	let myFullpage = new fullpage('#full');
+	var slideIndexBis = 1;
+	var sliding = false;
+	let myFullpage = new fullpage('#full')
 
+	$('.arr-d').click(function () {
+		myFullpage.moveSectionDown();
+	})
+	$('.arr-up').click(function () {
+		myFullpage.moveSectionUp();
+	})
 })
 $(document).ready(function () {
-	$('.preload').addClass('active')
+	$('.preload').fadeOut();
 })
+
+
 
